f8640a3aed607ea7ec605e03d902b40e9514c43a
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-06-18 14:37:22 +0200

    Style half of Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import css from "./layout.module.css"
 
 const roboto = Roboto({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700",],
+  weight: ["400", "500", "700"],
   variable: "--font-family",
   display: "swap",
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import styles from "./page.module.css";
+import Image from "next/image";
+import css from "./page.module.css";
 
 export default function Home() {
   return (
-		<main className={styles.main}>
-			
+		<main className={css.main}>
+			<div className={css.welcome_container}>
+				<div className={css.welcome_content}>
+					<h1>Unlock your potential with the best <span className={css.marked_text}><span>language</span></span> tutors</h1>
+					<p>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
+					<button>Get started</button>
+				</div>
+				<div className={css.welcome_image_container}>
+					<Image
+						src="/mac-smiling.jpg"
+						alt="Smiling girl hiding behind laptop"
+						width={568}
+						height={530}
+					/>
+				</div>
+			</div>
 		</main>
   );
 }
